79c7bb251c3055bbb973856bd3bdcfc7d3d656bc
Wan Cheuk Lun <winsonrich@gmail.com>
2022-05-11 16:14:21 +0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,15 +6,15 @@ public class Fibonacci {
         int n, i=0, c;
         Scanner s=new Scanner(System.in);
         n = s.nextInt();
-        for(c=1;c<=n;c++,i++){
+        for(c=1;c<=n;c++,i++){//for(i=0;i<n;i++){
             System.out.println(fibon(i));
         }
 
     }//end main
     public static int fibon(int in){
-        if(in==0||in==1){
+        if(in==0||in==1){//exclude the criteria of input 0 or 1 
             return in;
-        }else{
+        }else{//sum of previous number n-1 and n-2
             return (fibon(in-1)+fibon(in-2));
         }
     }
